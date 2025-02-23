35447e6aebc23bd12b23ee7149ac4a9d03b3c0bf
AudreyLican <audrey.lican@gmail.com>
2025-02-23 23:28:04 +0100

    Fisrt commit

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a singly linked list has a cycle.
+ * @list: Pointer to the head of the linked list.
+ * 
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *a, *b;
+
+    if (list == NULL || list->next == NULL)
+        return (0);
+        b = list;
+        a = list->next->next;
+    
+    while (b != a)
+    {
+        if (a == NULL || a->next == NULL)
+            return (0);
+        a = a->next->next;
+        b = b->next;
+    }
+
+    return (1);
+}
