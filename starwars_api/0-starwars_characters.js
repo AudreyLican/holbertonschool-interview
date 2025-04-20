b45c0922f4b4451976671f6b393c5a9536a8fad7
AudreyLican <audrey.lican@gmail.com>
2025-04-20 23:22:25 +0200

    fix

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,35 +1,34 @@
 #!/usr/bin/node
-
 const request = require('request');
 
 const filmID = process.argv[2];
 if (!filmID || isNaN(filmID)) {
-    console.log('Invalid film ID, ./0-starwars_characters.js <film_id>');
-    process.exit(1);
+  console.log('Invalid film ID, ./0-starwars_characters.js <film_id>');
+  process.exit(1);
 }
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + filmID;
 
 request(url, (error, response, body) => {
-    if (error) {
+  if (error) {
     console.error('Error: ', error);
     return;
-    }
+  }
 
-    const charData = JSON.parse(body).characters;
+  const charData = JSON.parse(body).characters;
 
-    const printChars = (index) => {
+  const printCharacters = (index) => {
     if (index >= charData.length) {
-        return;
+      return;
     }
     request(charData[index], (error, response, body) => {
-        if (error) {
+      if (error) {
         console.error('Error: ', error);
         return;
-        }
-        console.log(JSON.parse(body).name);
-        printCharrs(index + 1);
+      }
+      console.log(JSON.parse(body).name);
+      printCharacters(index + 1);
     });
-    };
-    printChars(0);
+  };
+  printCharacters(0);
 });
